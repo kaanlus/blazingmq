dcac9c78cea39f9315011e299af3e0e068eceaf1
kaanlus <120061216+kaanlus@users.noreply.github.com>
2024-04-24 19:19:32 +0000

    Copied over program so I can begin to write tests

---
+++ src/groups/wrappers/z_bmq/z_bmqa/z_bmqa_configurequeuestatus.t.cpp
@@ -0,0 +1,65 @@
+// Copyright 2023 Bloomberg Finance L.P.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// bmqa_configurequeuestatus.t.cpp                                    -*-C++-*-
+#include <z_bmqa_configurequeuestatus.h>
+
+// BMQ
+#include <bmqa_queueid.h>
+#include <bmqimp_queue.h>
+#include <bmqt_correlationid.h>
+#include <bmqt_uri.h>
+
+// MWC
+#include <mwcu_memoutstream.h>
+
+// BDE
+#include <bsl_memory.h>
+#include <bsl_string.h>
+
+// TEST DRIVER
+#include <mwctst_testhelper.h>
+
+// CONVENIENCE
+using namespace BloombergLP;
+using namespace bsl;
+
+
+
+// ============================================================================
+//                                 MAIN PROGRAM
+// ----------------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+    TEST_PROLOG(mwctst::TestHelper::e_DEFAULT);
+
+    bmqt::UriParser::initialize(s_allocator_p);
+
+    switch (_testCase) {
+    case 0:
+    case 3: test3_print(); break;
+    case 2: test2_comparison(); break;
+    case 1: test1_breathingTest(); break;
+    default: {
+        cerr << "WARNING: CASE '" << _testCase << "' NOT FOUND." << endl;
+        s_testStatus = -1;
+    } break;
+    }
+
+    bmqt::UriParser::shutdown();
+
+    TEST_EPILOG(mwctst::TestHelper::e_CHECK_DEF_GBL_ALLOC);
+}
